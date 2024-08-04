adfe09d74a06cf4fcc16c819bf4c7938efa33da6
Haruna Lawali <lawaliharuna943@gmail.com>
2024-08-05 00:50:24 +0100

    server.js
    Signed-off-by: Haruna Lawali  <lawaliharuna943@gmail.com>

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
+  cookie: { secure: false } // Set secure to false for local development
 }));
 
 // Middleware
